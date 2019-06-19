6ebe6f03c3c394c18b17edfa2de8edf5dd5ea6fc
Andrew Davis <atd285@gmail.com>
2019-06-19 14:20:23 -0400

    solution 10 refactor

--- arcade/10-commonCharacterCount.js
+++ arcade/10-commonCharacterCount.js
@@ -1,22 +1,19 @@
 function commonCharacterCount(s1, s2) {
   //TODO find the number of characters that appear in each, and take the minimum amount of each.
-  function getArrayLength(arr, char) {
-    return arr.filter((element) => element === char).length;
-  }
-  let freq1 = [];
-  let freq2 = [];
-  let a1 = s1.split("");
-  let a2 = s2.split("");
+  let freq1 = new Array(26).fill(0);
+  let freq2 = [...freq1];
   let commons = 0;
-  //initiate a for loop to cover the entire alphabet
-  for (let i = 0; i < 26; i++) {
-    let char = String.fromCharCode(i + "a".charCodeAt());
-    freq1.push(getArrayLength(a1, char));
-    freq2.push(getArrayLength(a2, char));
+
+  for (let char of s1) {
+    freq1[char.charCodeAt() - "a".charCodeAt()]++;
   }
-  for (let i = 0; i < 26; i++) {
-    commons += Math.min(freq1[i], freq2[i]);
+  for (let char of s2) {
+    freq2[char.charCodeAt() - "a".charCodeAt()]++;
   }
-  return commons;
+  return freq1
+    .map((str, indx) => Math.min(str, freq2[indx]))
+    .reduce((acc, curr) => {
+      return acc + curr;
+    });
 }
 console.log(commonCharacterCount("aabcc", "adcaa")); // 3
